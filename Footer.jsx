708d4a9acb007c69bfa1efbbc8c9bac83eb1b62a
ADITYA BHARDWAJ <80574773+aditya-75@users.noreply.github.com>
2021-06-27 23:27:23 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>copyright by Shape AI @ 2021{new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function info() {
+  return (
+    <div className="note">
+      <h1> Javascript and Reactjs</h1>
+      <p> A basic web dev React js Bootcamp</p>
+    </div>
+  );
+}
+export default info;
